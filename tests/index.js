b87dc523cc3e59d3c6fc032033480924e93f938a
alethéia <34745171+xirreal@users.noreply.github.com>
2022-01-19 10:51:00 +0100

    Update example plugin to use new wasm bundler syntax

--- tests/index.js
+++ tests/index.js
@@ -1,23 +1,11 @@
 import init, { testFunction, setup_hook } from '../pkg';
 
-import wasm from '../pkg/cc_binds_bg.wasm';
-
-// TODO: Add the base64 decode function automagically with the rollup plugin
-
-function _base64ToArrayBuffer(base64) {
-    var binary_string = window.atob(base64);
-    var len = binary_string.length;
-    var bytes = new Uint8Array(len);
-    for (var i = 0; i < len; i++) {
-        bytes[i] = binary_string.charCodeAt(i);
-    }
-    return bytes.buffer;
-}
+import importWasm from '../pkg/cc_binds_bg.wasm';
 
 export default (data) => {
     return {
         async onLoad() {
-            await init(_base64ToArrayBuffer(wasm));
+            await init(importWasm());
             setup_hook();
             console.log(testFunction()); // Should call cumcord.cum()
         },
